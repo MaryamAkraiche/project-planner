dd981d33b0c69d43384d7326fd0d856d12bf104b
Stacy Druart <stacy@StacyDruartMSI>
2024-05-07 12:04:42 +0200

    maj

--- assets/JS/darkmode.js
+++ assets/JS/darkmode.js
@@ -1,14 +1,13 @@
 // Créer le bouton pour basculer le mode sombre
-const modeBouton = document.createElement("button");
 const icon = document.createElement("i");
 
 // Ajouter la classe Font Awesome pour l'icône "lune" pour le mode sombre et "soleil" pour le mode clair
 icon.classList.add("fas");
 icon.classList.add("fa-sun"); // Par défaut, utilise l'icône de soleil pour le mode clair
-modeBouton.appendChild(icon);
+
 
 // Ajouter un gestionnaire d'événement pour basculer le mode sombre
-modeBouton.addEventListener('click', function() {
+icon.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
     icon.classList.remove("fa-sun");
@@ -19,6 +18,11 @@ modeBouton.addEventListener('click', function() {
   }
 });
 
-// Ajouter un identifiant pour le bouton
-modeBouton.setAttribute("id", "toggle-dark-mode");
-document.body.appendChild(modeBouton);
+// Ajouter un style CSS pour rendre l'icône cliquable
+icon.style.cursor = 'pointer';
+
+// Ajouter un identifiant pour l'icône
+icon.setAttribute("id", "toggle-dark-mode");
+
+// Ajouter l'icône à la fin du body
+document.body.appendChild(icon);
